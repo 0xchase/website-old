d7e8c6a9f63ce088a8e93425ea5d83d10c80a7c1
Chase Kanipe <32180077+0xchase@users.noreply.github.com>
2023-04-25 14:12:39 -0400

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
 function removeFocus() {
     document.getElementById('focus').classList.remove("focus");
 }
+
+function shrinkNavBar() {
+    var x = document.getElementById("myNavBar");
+    if (x.className === "navbar") {
+        x.className += " responsive";
+    } else {
+        x.className = "navbar";
+    }
+}
