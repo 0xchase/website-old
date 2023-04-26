756eb1850a41b4ccc4da874ccf125ead4f03b27b
0xchase <chasekanipe@gmail.com>
2023-04-26 14:19:12 -0400

    Fix for mobile button

--- script.js
+++ script.js
@@ -6,7 +6,11 @@ function shrinkNavBar() {
     var x = document.getElementById("myNavBar");
     if (x.className === "navbar") {
         x.className += " responsive";
-    } else {
+    } else if (x.className == "navbar wide-margin") {
+        x.className += " responsive";
+    } else if (x.className == "navbar responsive"){
         x.className = "navbar";
+    } else {
+        x.className = "navbar wide-margin";
     }
 }
