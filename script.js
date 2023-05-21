d9dc2a212eeb2149161560078b802ca8bc114bfa
0xchase <chasekanipe@gmail.com>
2023-05-21 15:51:10 -0400

    updates

--- script.js
+++ script.js
@@ -10,7 +10,26 @@ function shrinkNavBar() {
         x.className += " responsive";
     } else if (x.className == "navbar responsive"){
         x.className = "navbar";
+        hideAllMenus();
     } else {
         x.className = "navbar wide-margin";
+        hideAllMenus();
+    }
+}
+
+function hideAllMenus() {
+    document.getElementById("about").style.display = "";
+    document.getElementById("projects").style.display = "";
+    document.getElementById("music").style.display = "";
+}
+
+function toggleDisplay(name) {
+    if (window.innerWidth <= 600) {
+        var x = document.getElementById(name);
+        if (x.style.display.includes("block")) {
+            x.style.display = "";
+        } else {
+            x.style.display = "block";
+        }
     }
 }
